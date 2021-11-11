91ad00dec5750e16c51ea1ffd8b68781b48f6d64
shetty-codehub <kkshetty990@gmail.com>
2021-11-11 18:24:02 +0530

    cppclass

---
+++ Intro_class/class.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// class declaration
+class myclass {
+// private members to myclass
+int a;
+public:
+// public members to myclass
+void set_a(int num);
+int get_a( );
+};
+int main()
+{
+}
